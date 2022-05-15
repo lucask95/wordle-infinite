c215923b0763c0df5c0c197c7a98eefb02aea6b4
Lucas <ljkeller@ucdavis.edu>
2022-05-15 12:30:35 -0700

    Update python script

--- scripts/wordlist_frequency.py
+++ scripts/wordlist_frequency.py
@@ -2,16 +2,16 @@ from asyncore import write
 from typing import Set
 from wordfreq import zipf_frequency, word_frequency
 
-FILTER_NAMES = False
+FILTER_NAMES = True
 FREQUENCY_THRESHOLD = 2.4
 
 def write_wordsandfreq_to_json(filename, words_with_freq):
   with open(filename, 'w+') as file:
-    file.write('{\n    "items": [\n')
+    file.write('{\n    "items": {\n')
     for item in words_with_freq:
-      s = '"word": "{0}", "frequency": {1}'.format(item['word'], item['frequency'])
-      file.write('        { %s },\n' % s)
-    file.write('    ]\n}')
+      s = '        "{0}": {1},\n'.format(item['word'], item['frequency'])
+      file.write(s)
+    file.write('    }\n}')
 
 def word_to_entry(word):
   return {
@@ -28,8 +28,11 @@ def lines_to_list(filename):
 
 if __name__ == "__main__":
   word_list = lines_to_list('../data/wordlist.txt')
-  name_list = lines_to_list('../data/nameslist.txt')
   words_with_freq = [word_to_entry(word) for word in word_list]
   words_with_freq = list(filter(lambda entry: entry['frequency'] >= FREQUENCY_THRESHOLD, words_with_freq))
+  if FILTER_NAMES:
+    name_list = lines_to_list('../data/nameslist.txt')
+    name_set = set(name_list)
+    words_with_freq = list(filter(lambda entry: entry['word'] not in name_set, words_with_freq))
   words_with_freq.sort(key=lambda entry: entry['frequency'])
   write_wordsandfreq_to_json('../data/words_with_frequencies.json', words_with_freq)
